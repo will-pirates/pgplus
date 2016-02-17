73baf41caa492970effa6fe097243d68b55595be
Ashray <ashray.velapanur@gmail.com>
2016-02-17 18:14:19 +0530

    added header

--- home.py
+++ home.py
@@ -153,6 +153,7 @@ class GetTicketHandler(webapp2.RequestHandler):
         for note_id in t.note_ids:
             notes.append(self.read_note(note_id))
         response = {'id': t.key().id(), 'customer': t.customer, 'people': self.get_people(t.circle_id), 'engineer': t.engineer, 'lat': t.location.lat, 'lon': t.location.lon, 'documents': [self.get_document(document_id) for document_id in t.document_ids] , 'location_text': t.location_text, 'location': str(t.location), 'issue_type': t.issue_type, 'equipments': t.equipments, 'services': t.services, 'notes': notes}
+        self.response.headers.add_header("Access-Control-Allow-Origin", "*")
         self.response.write(json.dumps(response))
 
 class AssignCirclesHandler(webapp2.RequestHandler):
